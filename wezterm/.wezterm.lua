a818eeab0a3a7690b58072f9b770d390d43f814e
Matthew Schilling <mtthwschllng@gmail.com>
2026-04-16 01:44:37 -0400

    Update Wezterm Config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -8,18 +8,49 @@ config.hyperlink_rules = wezterm.default_hyperlink_rules()
 config.initial_cols = 128
 config.initial_rows = 28
 config.font_size = 13
-config.font = wezterm.font_with_fallback{"IntoneMono Nerd Font", "Intel One Mono"}
+config.font = wezterm.font("Intel One Mono")
 config.color_scheme = 'GruvboxDarkHard'
 
+config.keys = {
+  { key = 'LeftArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'RightArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+  { key = 'UpArrow',    mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'DownArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'h', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+}
+
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.5,
+}
+
 local is_windows = wezterm.target_triple:find("windows") ~= nil
+local is_macos = wezterm.target_triple:find("apple") ~= nil
 
 if is_windows then
+  config.font = wezterm.font_with_fallback{"IntoneMono Nerd Font", "Intel One Mono"}
   table.insert(launch_menu, {
     label = 'PowerShell',
     args = { 'powershell.exe', '-NoLogo' },
   })
   config.default_prog = { "C:\\Program Files\\Git\\bin\\bash.exe", "--login", "-i" }
 end
+
+if is_macos then
+  config.window_background_opacity = 0.75
+  config.send_composed_key_when_left_alt_is_pressed = false
+  config.send_composed_key_when_right_alt_is_pressed = false
+  config.quit_when_all_windows_are_closed = true
+
+  config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+
+  config.font = wezterm.font_with_fallback({
+    "Intel One Mono",
+    "SF Mono",
+  })
+end
+
 config.launch_menu = launch_menu
 
 return config
