2c0fd4e9fe01583d4835219c915361d59d4ea31f
Schilling, Matthew <matthew.schilling@intel.com>
2026-07-06 20:47:52 +0000

    Update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,9 +17,10 @@ config.keys = {
   { key = 'RightArrow', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
   { key = 'UpArrow',    mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
   { key = 'DownArrow',  mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
-  { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = 'h', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { key = 'v', mods = 'CTRL', action =wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'd', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 's', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = 'v', mods = 'CTRL|SHIFT', action = wezterm.action.PasteFrom 'Clipboard' },
+  { key = 'c', mods = 'CTRL|SHIFT', action = wezterm.action.CopyTo 'Clipboard' },
 }
 
 config.inactive_pane_hsb = {
@@ -31,16 +32,19 @@ local is_windows = wezterm.target_triple:find("windows") ~= nil
 local is_macos = wezterm.target_triple:find("apple") ~= nil
 
 if is_windows then
-  config.enable_kitty_keyboard = false
+  config.enable_kitty_keyboard = true
   config.font = wezterm.font_with_fallback{"IntoneMono Nerd Font", "Intel One Mono", "Cascadia Code"}
+
   table.insert(launch_menu, {
     label = 'PowerShell',
     args = { 'pwsh.exe', '-NoLogo' },
   })
+
   table.insert(launch_menu, {
     label = 'PowerShell (Old)',
     args = { 'powershell.exe', '-NoLogo' },
   })
+
   table.insert(launch_menu, {
     label = 'Git Bash',
     args = { 'C:\\Program Files\\Git\\bin\\bash.exe', '--login', '-i' },
@@ -59,7 +63,7 @@ if is_windows then
     local year, edition =
       vcvars:match('Microsoft Visual Studio[/\\](20%d%d)[/\\]([^/\\]+)')
     table.insert(launch_menu, {
-      label = 'x64 Native Tools VS ' .. year .. ' ' .. edition,
+      label = 'CMD x64 Native Tools VS ' .. year .. ' ' .. edition,
       args = { 'cmd.exe', '/k', 'C:/Program Files/' .. vcvars },
     })
   end
